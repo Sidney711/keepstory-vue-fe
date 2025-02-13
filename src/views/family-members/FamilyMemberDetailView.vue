c6589849de2614daba9ed6f23a2e2c0fb8e41f2c
Zdeněk Carbol <zdenek.carbol.711@gmail.com>
2025-02-13 08:49:30 +0100

    Add style for stories to familyMemberDetail

--- src/views/family-members/FamilyMemberDetailView.vue
+++ src/views/family-members/FamilyMemberDetailView.vue
@@ -22,20 +22,19 @@
               <v-img src="/avatar-blank.png" height="300px" alt="Profilový obrázek" />
 
               <v-card-title>
-                  <div class="flex gap-2 pb-2">
-                    <v-btn icon @click="editMember">
-                      <v-icon>mdi-pencil</v-icon>
-                    </v-btn>
-                    <v-btn icon @click="deleteMember">
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                    <v-btn icon @click="exportMember">
-                      <v-icon>mdi-file-pdf-box</v-icon>
-                    </v-btn>
-                  </div>
-                  <div class="text-h4">{{ member.firstName }} {{ member.lastName }}</div>
+                <div class="flex gap-2 pb-2">
+                  <v-btn icon @click="editMember">
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                  <v-btn icon @click="deleteMember">
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                  <v-btn icon @click="exportMember">
+                    <v-icon>mdi-file-pdf-box</v-icon>
+                  </v-btn>
+                </div>
+                <div class="text-h4">{{ member.firstName }} {{ member.lastName }}</div>
               </v-card-title>
-
 
               <v-card-subtitle>
                 {{ t('family.label.dateOfBirth') }}: {{ member.dateOfBirth }}
@@ -93,7 +92,30 @@
         <v-tab-item value="stories">
           <v-card flat v-if="activeTab === 'stories'">
             <v-card-text>
-              Zde patří příběhy
+              <div class="flex justify-end mb-4">
+                <v-btn color="primary" @click="addStory">
+                  <v-icon left>mdi-plus</v-icon>
+                  Nový příběh
+                </v-btn>
+              </div>
+
+              <div>
+                <v-card
+                  v-for="story in stories"
+                  :key="story.id"
+                  outlined
+                  class="mb-3 rounded-lg hover:shadow-lg cursor-pointer"
+                  @click="goToStoryDetail(story.id)"
+                >
+                  <v-card-text class="flex items-center justify-between">
+                    <div>
+                      <div class="text-h6 font-medium">{{ story.title }}</div>
+                      <div class="text-body-2 text-gray-600">{{ story.date }}</div>
+                    </div>
+                    <v-icon>mdi-chevron-right</v-icon>
+                  </v-card-text>
+                </v-card>
+              </div>
             </v-card-text>
           </v-card>
         </v-tab-item>
@@ -104,13 +126,14 @@
 
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import AppLayout from '@/layouts/AppLayout.vue'
 import { useFamilyMembersStore } from '@/stores/familyMemberStore'
 
 const { t } = useI18n()
 const route = useRoute()
+const router = useRouter()
 const familyStore = useFamilyMembersStore()
 
 const memberId = route.params.id as string
@@ -137,6 +160,27 @@ const deleteMember = () => {
 
 const exportMember = () => {
   console.log("Export member:", member.value)
+}
+
+// Statická data pro příběhy
+interface Story {
+  id: string
+  title: string
+  date: string
+}
+
+const stories = ref<Story[]>([
+  { id: '1', title: 'První příběh', date: '2025-01-01' },
+  { id: '2', title: 'Druhý příběh', date: '2025-02-15' },
+  { id: '3', title: 'Třetí příběh', date: '2025-03-10' }
+])
+
+const addStory = () => {
+  console.log('Přidat nový příběh')
+}
+
+const goToStoryDetail = (id: string) => {
+  console.log('Navigace do detailu příběhu s id:', id)
 }
 </script>
 
